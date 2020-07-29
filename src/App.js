2221492e35c0c2816fc0986cccece72472280eb8
madhav3099 <madhavendrasingh3099@gmail.com>
2020-07-29 22:09:37 +0530

    counter app added without styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{Component} from 'react';
+
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+        count: 0
+    }
+  }
+
+calculate(c){
+    var count1 = this.state.count
+    if (c === "+"){
+       count1 += 1
+    }else{
+      count1 -=1
+    }
+  this.setState({count: count1})
+   
+}
+  
+
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="s1">
+      <h1>Counter</h1>
+     <div className="s"><p>{this.state.count}</p></div>
+     <br></br>
+     <button className="btn1" onClick = {() => this.calculate("+")}>+</button>
+     <button className="btn1" onClick = {() => this.calculate("-")}>-</button>
+
+
+
     </div>
+
+    
   );
 }
+
+}
+
 
 export default App;
